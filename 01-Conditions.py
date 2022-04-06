1ea52b7c5b1ca6582d61bc97cb01cca30e0b9a91
Mohammednamara <mohammedhnamara@gmail.com>
2022-04-06 16:02:46 +0300

    Adding Python Conditions

---
+++ 01-Conditions.py
@@ -0,0 +1,38 @@
+# Simple Condition
+
+x= 5
+if x<2:
+    print ("x>2")
+else:
+    print (x)
+
+
+  
+# if elif else
+
+x= 600
+
+if x<400:
+    if x<340:
+        print('The value <340')
+        
+    else:
+        print ('The Value>340')
+        
+elif x > 500 :
+        print ('x>500')
+        
+else:
+        print ('The Value between 400 and 500')
+
+
+# if with boolean Operators
+Name = 'Mohammed'
+Age = 28
+
+if Name =='Mohammed' and Age ==28:
+    print('Ok')
+    
+if Name =='Omar' or Age ==28:
+    print ('Hello')
+    
